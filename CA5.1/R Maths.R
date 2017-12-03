3516ead803063be937e791c8f9e2ad0be4aa1eb9
Chellser <fahy.michelle@gmail.com>
2017-12-03 13:40:32 +0000

    Adding R Code for Part 1 of CA5

---
+++ CA5.1/R Maths.R
@@ -0,0 +1,46 @@
+#Create maths functions based on CA1
+
+
+add_two_numbers <- function(num1, num2){num1 + num2}
+#test the function
+add_two_numbers(5,6)
+
+subtract_two_numbers <- function(num1, num2){num1 - num2}
+#test the function
+subtract_two_numbers(5,6)
+
+multiply_two_numbers <- function(num1, num2){num1 * num2}
+#test the function
+multiply_two_numbers(5,6)
+
+divide_two_numbers <- function(num1, num2){num1 / num2}
+#test the function
+divide_two_numbers(5,6)
+
+exponential_two_numb <- function(numb1, numb2){numb1 ** numb2}
+#test the function
+exponential_two_numb(5,3)
+
+cube <- function(numb1){numb1 * numb1 * numb1}
+#test the function
+cube(10)
+
+sine <- function(numb1) {
+    sin(numb1)
+}
+#test the function
+sine(5)
+
+my_cos <- function(numb1) {
+  cos(numb1)
+}
+#test the function
+my_cos(10)
+
+my_tan <- function(numb1) {
+  tan(numb1)
+}
+#test the function
+my_tan(8)
+
+
